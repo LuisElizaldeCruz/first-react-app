5156f5a8006737dcbb2d6381b66693f2d2ace382
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2022-08-02 23:11:06 -0500

    se agrego los tres componentes de comentarios

--- src/App.js
+++ src/App.js
@@ -6,7 +6,30 @@ function App() {
     <div className="App">
       <div className="contenedor-principal">
         <h1>Esto es lo que dicen nuestros alumnos acerca de freeCodeCamp</h1>
-        <Testimonio />
+        <Testimonio
+          nombre="Shawn Wang" 
+          pais="Singapur"
+          imagen="shawn"
+          cargo="Ingeniero de software"
+          empresa="Amazon"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit magnam consectetur aliquam, illo ratione sit qui voluptatem, esse praesentium rem repellat impedit sed vitae eligendi fugiat. Aliquam perspiciatis dicta esse."
+          />
+          <Testimonio
+          nombre="Sarah Chima"
+          pais="Nigeria"
+          imagen="sarah"
+          cargo="Ingeniera de software"
+          empresa="ChatDesk"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit magnam consectetur aliquam, illo ratione sit qui voluptatem, esse praesentium rem repellat impedit sed vitae eligendi fugiat. Aliquam perspiciatis dicta esse."
+          />
+        <Testimonio
+          nombre="Emma Bostian"
+          pais="Suecia"
+          imagen="emma"
+          cargo="Ingeniero de Software"
+          empresa="Spotify"
+          testimonio="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit magnam consectetur aliquam, illo ratione sit qui voluptatem, esse praesentium rem repellat impedit sed vitae eligendi fugiat. Aliquam perspiciatis dicta esse."
+        />
       </div>
     </div>
   );
--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -6,15 +6,16 @@ const Testimonio =(props)=>{
         <div className="contenedor-testimonio">
             <img 
             className="imagen-testimonio"
-            src={require("../imagenes/imagen-emma.png")} 
-            alt="foto de perfil"/>
+            src={require(`../imagenes/testimonio-${props.imagen}.png`)} 
+            alt={`foto de ${props.imagen}`}/>
             <div className="contenedor-texto-testimonio">   
-                <p className="nombre-testimonio">{props.nombre} Bostian en {props.pais}</p>
-                <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+                <p className="nombre-testimonio"><strong>{props.nombre}</strong> en {props.pais}</p>
+                <p className="cargo-testimonio">{props.cargo} en <strong>{props.empresa}</strong></p>
                 <p className="texto-testimonio">"{props.testimonio}".</p>
             </div>
         </div>
     );
 }
+
 
 export default Testimonio;
